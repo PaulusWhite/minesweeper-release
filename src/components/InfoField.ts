a849c593ae48ee910bef08b861910adb9573a924
Pavel <pauledelweissontop@gmail.com>
2024-01-02 19:00:42 +0300

    feat:adjust component for futher work

--- src/components/InfoField.ts
+++ src/components/InfoField.ts
@@ -3,15 +3,33 @@ const InfoField = () => {
   <div class="info-field">
 
     <div class="info-field__level-info">
-      <span class="info-field__bombs-amount info-field__indicator">10</span>
-      <span class="info-field__game-layout info-field__indicator">15 x 15</span>
+
+      <span class="info-field__bombs-info info-field__indicator">
+        <span class="info-field__bomb-icon"></span>
+        <span class="info-field__bombs-quantity"></span>
+      </span>
+
       <button class="info-field__restart-btn info-field__indicator">restart</button>
+      
     </div>
 
     <div class="info_field__game-info">
-      <span class="info-field__time-counter info-field__indicator">00.00</span>
-      <span class="info-field__moves-counter info-field__indicator">0</span>
-      <span class="info-field__flags-counter info-field__indicator">0</span>
+
+      <span class="info-field__time-counter info-field__indicator">
+        <span class="info-field__time-counter-icon"></span>
+        <span class="info-field__time-counter-value"></span>
+      </span>
+
+      <span class="info-field__moves-counter info-field__indicator">
+        moves
+        <span class="info-field__moves-counter-value"></span>
+      </span>
+
+      <span class="info-field__flags-counter info-field__indicator">
+        <span class="info-field__flags-counter-icon"></span>
+        <span class="info-field__flags-counter-value"></span>
+      </span>
+
     </div>
 
   </div>
