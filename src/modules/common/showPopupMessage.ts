55b52523974eb52aa041a526fde28f4c3e36ebb3
Pavel <pauledelweissontop@gmail.com>
2024-02-18 22:22:48 +0300

    fix: fix popup display

--- src/modules/common/showPopupMessage.ts
+++ src/modules/common/showPopupMessage.ts
@@ -2,9 +2,7 @@ const showPopupMessage = (messageText: string) => {
   const popup: HTMLDivElement = document.querySelector(".popup") as HTMLDivElement;
   const messageField: HTMLParagraphElement = popup.firstElementChild as HTMLParagraphElement;
   messageField.textContent = messageText;
-
-  if (popup.classList.contains("show-popup")) return;
-
+  
   popup.classList.add("show-popup");
 
   setTimeout(() => {
