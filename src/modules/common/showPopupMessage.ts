5740c08f8443244c7b79fa8d827d17d6b5588e06
Pavel <pauledelweissontop@gmail.com>
2024-03-17 00:55:42 +0300

    feat: improve sidebar ui

--- src/modules/common/showPopupMessage.ts
+++ src/modules/common/showPopupMessage.ts
@@ -1,6 +1,6 @@
 const POPUP_TIMER_NAME: string = "popupTimer";
 
-const showPopupMessage = (messageText: string, timer: number = 5000) => {
+const showPopupMessage = (messageText: string, timer: number = 3500) => {
   const popup: HTMLDivElement = document.querySelector(".popup") as HTMLDivElement;
   const messageField: HTMLParagraphElement = popup.firstElementChild as HTMLParagraphElement;
 
